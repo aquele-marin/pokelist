03b48f2af74fa48adda4c11dcad69b2fdb7b1c09
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-07-17 15:31:57 -0300

    add atom design pattern && add pagination system && add authentication preps

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,58 @@
-import Home from './pages/home/index';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Home from "./components/pages/home/index";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { ProvideAuth } from "./hooks/useAuth";
+import ScrollToTop from "./utils/ScrollToTop";
+import { CssBaseline } from "@mui/material";
+import Login from "./components/pages/login/";
+import Landing from "./components/pages/landing/";
+
+function PrivateRoute({ children, path, exact = false, ...rest }) {
+    // const { refreshToken } = useAuth();
+    const refreshToken = "Bearer Ad3rrfbvf3efgvrfr3rvfevre";
+
+    return (
+        <Route
+            {...rest}
+            exact={exact}
+            path={path}
+            render={({ location }) => {
+                return refreshToken ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: {
+                                from: location,
+                            },
+                        }}
+                    />
+                );
+            }}
+        />
+    );
+}
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <ProvideAuth>
+            <BrowserRouter>
+                <ScrollToTop />
+                <CssBaseline />
+                <Switch>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                    <Route exact path="/">
+                        <Landing />
+                    </Route>
+                    <PrivateRoute exact path="/home">
+                        <Home />
+                    </PrivateRoute>
+                </Switch>
+            </BrowserRouter>
+        </ProvideAuth>
+    );
 }
 
 export default App;
---
+++ src/constants/messages.jsx
@@ -0,0 +1,2 @@
+export const REQUEST_ERROR_401 =
+    "You are not authorized to access this resource.";
