11156be9e9c900d435bf7362cd12a997a4a5a413
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-08-23 23:43:24 -0300

    add: create daisyUI darkmode and remove others dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {
-    CssBaseline,
-    ThemeProvider,
-    createTheme,
-    useMediaQuery,
-} from "@mui/material";
+// import { ThemeProvider, createTheme, useMediaQuery } from "@mui/material";
 import { Login } from "./components/pages/login";
 import { Landing } from "./components/pages/landing";
 import { Main } from "./components/pages/main";
@@ -64,60 +59,30 @@ function Home() {
 }
 
 function App() {
-    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-    const [mode, setMode] = useState<"light" | "dark">(
+    // document.documentElement.setAttribute("data-theme", themesArray[0]);
+    // window.matchMedia('(prefers-color-scheme: dark)').matches
+
+    const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    ).matches;
+
+    const mode =
         localStorage.theme !== undefined
             ? localStorage.theme
             : prefersDarkMode
             ? "dark"
-            : "light"
-    );
-    const colorMode = useMemo(
-        () => ({
-            toggleColorMode: () => {
-                setMode((prevMode) =>
-                    prevMode === "light" ? "dark" : "light"
-                );
-            },
-        }),
-        []
-    );
+            : "light";
 
-    const theme = useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    mode,
-                },
-            }),
-        [mode]
-    );
-
-    useEffect(() => {
-        if (!("theme" in localStorage)) {
-            localStorage.theme = mode;
-        }
-
-        if (mode === "dark") {
-            localStorage.theme = "dark";
-            document.documentElement.classList.add("dark");
-        } else {
-            localStorage.theme = "light";
-            document.documentElement.classList.remove("dark");
-        }
-    }, [mode]);
+    localStorage.theme = mode;
+    document.documentElement.setAttribute("data-theme", mode);
 
     return (
         <BrowserRouter>
-            <ColorModeContext.Provider value={colorMode}>
-                <ThemeProvider theme={theme}>
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/landing" element={<Landing />} />
-                        <Route path="/*" element={<Home />} />
-                    </Routes>
-                </ThemeProvider>
-            </ColorModeContext.Provider>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/landing" element={<Landing />} />
+                <Route path="/*" element={<Home />} />
+            </Routes>
         </BrowserRouter>
     );
 }
--- src/components/atoms/Header/Header.tsx
+++ src/components/atoms/Header/Header.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { SwapDarkMode } from "./SwapDarkMode";
+
 export function Header() {
     const renderMobileMenu = () => (
         <div className="dropdown">
@@ -22,21 +25,7 @@ export function Header() {
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
                 <li>
-                    <a>Item 1</a>
-                </li>
-                <li>
-                    <a>Parent</a>
-                    <ul className="p-2">
-                        <li>
-                            <a>Submenu 1</a>
-                        </li>
-                        <li>
-                            <a>Submenu 2</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a>Item 3</a>
+                    <Link to="/pokedex">Pokedex</Link>
                 </li>
             </ul>
         </div>
@@ -46,23 +35,7 @@ export function Header() {
         <div className="hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
                 <li>
-                    <a>Item 1</a>
-                </li>
-                <li tabIndex={0}>
-                    <details>
-                        <summary>Parent</summary>
-                        <ul className="p-2">
-                            <li>
-                                <a>Submenu 1</a>
-                            </li>
-                            <li>
-                                <a>Submenu 2</a>
-                            </li>
-                        </ul>
-                    </details>
-                </li>
-                <li>
-                    <a>Item 3</a>
+                    <Link to="/pokedex">Pokedex</Link>
                 </li>
             </ul>
         </div>
@@ -72,7 +45,9 @@ export function Header() {
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 {renderMobileMenu()}
-                <a className="btn btn-ghost normal-case text-xl">PokeList</a>
+                <Link className="btn btn-ghost normal-case text-xl" to="/">
+                    PokeList
+                </Link>
                 {renderMenu()}
             </div>
             <div className="navbar-end flex-none gap-2">
@@ -97,19 +72,16 @@ export function Header() {
                         className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
                     >
                         <li>
-                            <a className="justify-between">
+                            <Link className="justify-between" to="/profile">
                                 Profile
-                                <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a>Settings</a>
-                        </li>
-                        <li>
-                            <a>Logout</a>
+                            <Link to="/login">Logout</Link>
                         </li>
                     </ul>
                 </div>
+                <SwapDarkMode />
             </div>
         </div>
     );
