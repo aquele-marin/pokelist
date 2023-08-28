ce695a66905d0a90da9c078af8ec175bcd108981
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-08-28 00:00:10 -0300

    fix:  MinimalistFooter removal

--- src/components/atoms/Footer/index.tsx
+++ src/components/atoms/Footer/index.tsx
@@ -1,12 +1,1 @@
 export * from "./Footer";
-import {
-    MinimalistFooterComponent,
-    MFCDescription,
-    MFCTitle,
-} from "./MinimalistFooter";
-
-export const MinimalistFooter = {
-    Component: MinimalistFooterComponent,
-    Title: MFCTitle,
-    Description: MFCDescription,
-};
